d1fc8bf2a8ed804d14f6052c5f06278bfae96858
sourishg <ragnarok0211@gmail.com>
2016-07-03 19:13:43 -0400

    Add popt to fisheye stereo calibration

--- calibrate.cpp
+++ calibrate.cpp
@@ -4,34 +4,27 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <stdio.h>
 #include <iostream>
+#include "popt_pp.h"
 
 using namespace std;
 using namespace cv;
 
-int board_w = 9;
-int board_h = 6;
-float squareSize = 0.02423; //0.02423m
-
-cv::Size board_sz = cv::Size(board_w, board_h);
-int board_n = board_w * board_h;
-
-vector<vector<cv::Point3d> > object_points;
-vector<vector<cv::Point2f> > imagePoints1, imagePoints2;
-vector<cv::Point2f> corners1, corners2;
+vector< vector< Point3d > > object_points;
+vector< vector< Point2f > > imagePoints1, imagePoints2;
+vector< Point2f > corners1, corners2;
 vector< vector< Point2d > > left_img_points, right_img_points;
 
-Mat img1, img2, gray1, gray2, spl1, spl2, Fimg1, Fimg2;
+Mat img1, img2, gray1, gray2, spl1, spl2;
 
-void load_image_points() {
-  int num_imgs = 29;
-
-  spl1 = imread("/Users/sourishghosh/vision/fisheye_stereo/imgs/left10.jpg", CV_LOAD_IMAGE_COLOR);
-  spl2 = imread("/Users/sourishghosh/vision/fisheye_stereo/imgs/right10.jpg", CV_LOAD_IMAGE_COLOR);
+void load_image_points(int board_width, int board_height, float square_size, int num_imgs, 
+                      char* img_dir, char* leftimg_filename, char* rightimg_filename) {
+  Size board_size = Size(board_width, board_height);
+  int board_n = board_width * board_height;
 
   for (int i = 1; i <= num_imgs; i++) {
     char left_img[100], right_img[100];
-    sprintf(left_img, "/Users/sourishghosh/vision/fisheye_stereo/imgs/left%d.jpg", i);
-    sprintf(right_img, "/Users/sourishghosh/vision/fisheye_stereo/imgs/right%d.jpg", i);
+    sprintf(left_img, "%s%s%d.jpg", img_dir, leftimg_filename, i);
+    sprintf(right_img, "%s%s%d.jpg", img_dir, rightimg_filename, i);
     img1 = imread(left_img, CV_LOAD_IMAGE_COLOR);
     img2 = imread(right_img, CV_LOAD_IMAGE_COLOR);
     cv::cvtColor(img1, gray1, CV_BGR2GRAY);
@@ -39,28 +32,28 @@ void load_image_points() {
 
     bool found1 = false, found2 = false;
 
-    found1 = cv::findChessboardCorners(img1, board_sz, corners1,
+    found1 = cv::findChessboardCorners(img1, board_size, corners1,
   CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
-    found2 = cv::findChessboardCorners(img2, board_sz, corners2,
+    found2 = cv::findChessboardCorners(img2, board_size, corners2,
   CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
 
     if (found1)
     {
       cv::cornerSubPix(gray1, corners1, cv::Size(5, 5), cv::Size(-1, -1),
   cv::TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
-      cv::drawChessboardCorners(gray1, board_sz, corners1, found1);
+      cv::drawChessboardCorners(gray1, board_size, corners1, found1);
     }
     if (found2)
     {
       cv::cornerSubPix(gray2, corners2, cv::Size(5, 5), cv::Size(-1, -1),
   cv::TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
-      cv::drawChessboardCorners(gray2, board_sz, corners2, found2);
+      cv::drawChessboardCorners(gray2, board_size, corners2, found2);
     }
 
     vector<cv::Point3d> obj;
-    for( int i = 0; i < board_h; ++i )
-      for( int j = 0; j < board_w; ++j )
-        obj.push_back(Point3d(double( (float)j * squareSize ), double( (float)i * squareSize ), 0));
+    for( int i = 0; i < board_height; ++i )
+      for( int j = 0; j < board_width; ++j )
+        obj.push_back(Point3d(double( (float)j * square_size ), double( (float)i * square_size ), 0));
 
     if (found1 && found2) {
       cout << i << ". Found corners!" << endl;
@@ -82,7 +75,31 @@ void load_image_points() {
 
 int main(int argc, char const *argv[])
 {
-  load_image_points();
+  int board_width, board_height, num_imgs;
+  float square_size;
+  char* img_dir;
+  char* leftimg_filename;
+  char* rightimg_filename;
+  char* out_file;
+
+  static struct poptOption options[] = {
+    { "board_width",'w',POPT_ARG_INT,&board_width,0,"Checkerboard width","NUM" },
+    { "board_height",'h',POPT_ARG_INT,&board_height,0,"Checkerboard height","NUM" },
+    { "square_size",'s',POPT_ARG_FLOAT,&square_size,0,"Checkerboard square size","NUM" },
+    { "num_imgs",'n',POPT_ARG_INT,&num_imgs,0,"Number of checkerboard images","NUM" },
+    { "img_dir",'d',POPT_ARG_STRING,&img_dir,0,"Directory containing images","STR" },
+    { "leftimg_filename",'l',POPT_ARG_STRING,&leftimg_filename,0,"Left image prefix","STR" },
+    { "rightimg_filename",'r',POPT_ARG_STRING,&rightimg_filename,0,"Right image prefix","STR" },
+    { "out_file",'o',POPT_ARG_STRING,&out_file,0,"Output calibration filename (YML)","STR" },
+    POPT_AUTOHELP
+    { NULL, 0, 0, NULL, 0, NULL, NULL }
+  };
+
+  POpt popt(NULL, argc, argv, options, 0);
+  int c;
+  while((c = popt.getNextOpt()) >= 0) {}
+
+  load_image_points(board_width, board_height, square_size, num_imgs, img_dir, leftimg_filename, rightimg_filename);
 
   printf("Starting Calibration\n");
   cv::Matx33d K1, K2, R;
@@ -99,7 +116,7 @@ int main(int argc, char const *argv[])
       K1, D1, K2, D2, img1.size(), R, T, flag,
       cv::TermCriteria(3, 12, 0));
 
-  cv::FileStorage fs1("mystereocalib.yml", cv::FileStorage::WRITE);
+  cv::FileStorage fs1(out_file, cv::FileStorage::WRITE);
   fs1 << "K1" << Mat(K1);
   fs1 << "K2" << Mat(K2);
   fs1 << "D1" << D1;
@@ -121,19 +138,5 @@ Q, CV_CALIB_ZERO_DISPARITY, img1.size(), 0.0, 1.1);
   fs1 << "Q" << Q;
 
   printf("Done Rectification\n");
-
-  cv::Mat lmapx, lmapy, rmapx, rmapy;
-  Mat imgU1, imgU2;
-  cv::fisheye::initUndistortRectifyMap(K1, D1, R1, P1, spl1.size(), CV_32F, lmapx, lmapy);
-  cv::fisheye::initUndistortRectifyMap(K2, D2, R2, P2, spl1.size(), CV_32F, rmapx, rmapy);
-
-  cv::remap(spl1, imgU1, lmapx, lmapy, cv::INTER_LINEAR);
-  cv::remap(spl2, imgU2, rmapx, rmapy, cv::INTER_LINEAR);
-
-  imshow("image1", imgU1);
-  imshow("image2", imgU2);
-
-  waitKey(0);
-
   return 0;
 }
